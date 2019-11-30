238230847878018a7f7b3d485c3df6d82e4e5ca2
No2 <noe.tiger@free.fr>
2019-11-30 16:08:27 +0000

    adding multi object rendering

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,78 @@ using namespace glm;
 #include "controls.hpp"
 #include "loader.hpp"
 
+/*
+MatrixID, ModelMatrixID, ViewMatrixID, LightID, TextureID, vertices
+*/
+
+void draw(glm::vec3 gOrientation, glm::vec3 gPosition, glm::vec3 gScale, GLuint vertexbuffer, GLuint uvbuffer, GLuint normalbuffer, GLuint MatrixID, GLuint ModelMatrixID, GLuint ViewMatrixID, GLuint LightID, GLuint TextureID, GLuint Texture, std::vector<glm::vec3> vertices) {
+		glm::mat4 ProjectionMatrix = getProjectionMatrix();
+		glm::mat4 ViewMatrix = getViewMatrix();
+
+		glm::mat4 RotationMatrix = eulerAngleYXZ(gOrientation.y, gOrientation.x, gOrientation.z);
+		glm::mat4 TranslationMatrix = glm::translate(RotationMatrix, gPosition);
+		glm::mat4 ScalingMatrix = glm::scale(TranslationMatrix, gScale);
+		glm::mat4 ModelMatrix =  ScalingMatrix;
+
+		// ProjectionMatrix *= ;
+		glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+
+		// Send our transformation to the currently bound shader, 
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+		glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+		glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
+
+		glm::vec3 lightPos = glm::vec3(4,4,4);
+		glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
+
+		// Bind our texture in Texture Unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, Texture);
+		// Set our "myTextureSampler" sampler to use Texture Unit 0
+		glUniform1i(TextureID, 0);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// 2nd attribute buffer : UVs
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+		glVertexAttribPointer(
+			1,                                // attribute
+			2,                                // size
+			GL_FLOAT,                         // type
+			GL_FALSE,                         // normalized?
+			0,                                // stride
+			(void*)0                          // array buffer offset
+		);
+
+		// 3rd attribute buffer : normals
+		glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+		glVertexAttribPointer(
+			2,                                // attribute
+			3,                                // size
+			GL_FLOAT,                         // type
+			GL_FALSE,                         // normalized?
+			0,                                // stride
+			(void*)0                          // array buffer offset
+		);
+
+		// Draw the triangles !
+		glDrawArrays(GL_TRIANGLES, 0, vertices.size() );
+
+}
+
 int main( void )
 {
 	// Initialise GLFW
@@ -98,8 +170,11 @@ int main( void )
 	std::vector<glm::vec2> uvs;
 	std::vector<glm::vec3> normals;
 	bool res = loadOBJ("../sources/suzanne.obj", vertices, uvs, normals);
+	std::vector<glm::vec3> vertices2;
+	std::vector<glm::vec2> uvs2;
+	std::vector<glm::vec3> normals2;
+	bool res2 = loadOBJ("../sources/suzanne.obj", vertices2, uvs2, normals2);
 
-	// Load it into a VBO
 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
@@ -116,6 +191,21 @@ int main( void )
 	glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
 	glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals[0], GL_STATIC_DRAW);
 
+	GLuint vertexbuffer2;
+	glGenBuffers(1, &vertexbuffer2);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer2);
+	glBufferData(GL_ARRAY_BUFFER, vertices2.size() * sizeof(glm::vec3), &vertices2[0], GL_STATIC_DRAW);
+
+	GLuint uvbuffer2;
+	glGenBuffers(1, &uvbuffer2);
+	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer2);
+	glBufferData(GL_ARRAY_BUFFER, uvs2.size() * sizeof(glm::vec2), &uvs2[0], GL_STATIC_DRAW);
+
+	GLuint normalbuffer2;
+	glGenBuffers(1, &normalbuffer2);
+	glBindBuffer(GL_ARRAY_BUFFER, normalbuffer2);
+	glBufferData(GL_ARRAY_BUFFER, normals2.size() * sizeof(glm::vec3), &normals2[0], GL_STATIC_DRAW);
+
 	// Get a handle for our "LightPosition" uniform
 	glUseProgram(programID);
 	GLuint LightID = glGetUniformLocation(programID, "LightPosition_worldspace");
@@ -124,6 +214,10 @@ int main( void )
 	vec3 gOrientation(1.0, 1.0, 1.0);
 	vec3 gPosition(-1.0, -1.0, -1.0);
 	vec3 gScale(1.0, 1.0, 1.0);
+
+	vec3 gOrientation2(-1.0, -1.0, -1.0);
+	vec3 gPosition2(1.0, 1.0, 1.0);
+	vec3 gScale2(1.0, 1.0, 1.0);
 
 	double lastTime = glfwGetTime();
 	double lastFrameTime = lastTime;
@@ -146,80 +240,31 @@ int main( void )
 
 		// Compute the MVP matrix from keyboard and mouse input
 		computeMatricesFromInputs();
-		glm::mat4 ProjectionMatrix = getProjectionMatrix();
-		glm::mat4 ViewMatrix = getViewMatrix();
+
 		gOrientation.y += 0.0031415/ 2.0f * deltaTime;
 		gOrientation.x += 0.0031415/ 2.0f * deltaTime;
 		gOrientation.z += 0.0031415/ 2.0f * deltaTime;
 		gPosition.x += 0.001 * deltaTime;
 		gPosition.y += 0.001 * deltaTime;
 		gPosition.z += 0.001 * deltaTime;
-		gScale.x -= 0.1 * deltaTime;
-		gScale.y -= 0.1 * deltaTime;
-		gScale.z -= 0.1 * deltaTime;
+		// gScale.x -= 0.1 * deltaTime;
+		// gScale.y -= 0.1 * deltaTime;
+		// gScale.z -= 0.1 * deltaTime;
 
-		glm::mat4 RotationMatrix = eulerAngleYXZ(gOrientation.y, gOrientation.x, gOrientation.z);
-		glm::mat4 TranslationMatrix = glm::translate(RotationMatrix, gPosition);
-		glm::mat4 ScalingMatrix = glm::scale(TranslationMatrix, gScale);
-		glm::mat4 ModelMatrix =  ScalingMatrix;
+		gOrientation2.y -= 0.0031415/ 2.0f * deltaTime;
+		gOrientation2.x -= 0.0031415/ 2.0f * deltaTime;
+		gOrientation2.z -= 0.0031415/ 2.0f * deltaTime;
+		gPosition2.x -= 0.001 * deltaTime;
+		gPosition2.y -= 0.001 * deltaTime;
+		gPosition2.z -= 0.001 * deltaTime;
+		// gScale.x -= 0.1 * deltaTime;
+		// gScale.y -= 0.1 * deltaTime;
+		// gScale.z -= 0.1 * deltaTime;
 
-		// ProjectionMatrix *= ;
-		glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
 
-		// Send our transformation to the currently bound shader, 
-		// in the "MVP" uniform
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-		glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
-		glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
-
-		glm::vec3 lightPos = glm::vec3(4,4,4);
-		glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
-
-		// Bind our texture in Texture Unit 0
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, Texture);
-		// Set our "myTextureSampler" sampler to use Texture Unit 0
-		glUniform1i(TextureID, 0);
-
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			0,                  // attribute
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
-
-		// 2nd attribute buffer : UVs
-		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-		glVertexAttribPointer(
-			1,                                // attribute
-			2,                                // size
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
-		);
-
-		// 3rd attribute buffer : normals
-		glEnableVertexAttribArray(2);
-		glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
-		glVertexAttribPointer(
-			2,                                // attribute
-			3,                                // size
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
-		);
-
-		// Draw the triangles !
-		glDrawArrays(GL_TRIANGLES, 0, vertices.size() );
-
+		draw(gOrientation, gPosition, gScale, vertexbuffer, uvbuffer, normalbuffer, MatrixID, ModelMatrixID, ViewMatrixID, LightID, TextureID, Texture, vertices);
+		draw(gOrientation2, gPosition2, gScale2, vertexbuffer2, uvbuffer2, normalbuffer2, MatrixID, ModelMatrixID, ViewMatrixID, LightID, TextureID, Texture, vertices2);
+		
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
 		glDisableVertexAttribArray(2);
